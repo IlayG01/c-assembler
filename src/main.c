af7d7049242b181e71a19346f8d77390270c96d4
Ido Ben Hamo <idobhh@gmail.com>
2025-04-01 22:05:31 +0300

    update todo

--- src/main.c
+++ src/main.c
@@ -8,14 +8,11 @@
 #define MINIMUM_ARGS 2
 
 /* TODOs */
-/* strip whitespaces - between opcodes operands*/
-/* walkthrough the error handling in the assebler flow - only fatal errors should stop the program(files access), other should be printed and continue with a flag to not write output files */
-/* move out utils functions into utils module */
-/* order the code - consts, efficiency, general order*/
-/* docs at assembler.c */
-/* Fix Warnings */
+/* data and externals should be aloocated dynamically */
+/* docs */
+/* Fix Warnings in pre processor */
 /* Add examples for input and output files of the program (images), working example and failures with appropriate errors prints */
-/* main.c */
+/* update main.c */
 
 int main(int argc, char* argv[]) {
     int i;
